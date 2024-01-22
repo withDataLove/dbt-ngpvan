fb3681134e5aed255744b090adc8de264a880227
Michael Brewster <brew@withdatalove.com>
2024-01-22 14:28:42 +0000

    updating 02

--- models/intermediate/ngpvan_classic/int_ngpvan_classic__02__contributions_with_compliance_data.sql
+++ models/intermediate/ngpvan_classic/int_ngpvan_classic__02__contributions_with_compliance_data.sql
@@ -14,10 +14,30 @@ compliance AS (
     FROM {{ ref('int_ngpvan_classic__01__contributions_compliance_denormalized') }}
 )
 
-SELECT surrogate_contacts_contribution_id
-FROM contributions
-LEFT JOIN source_codes USING (surrogate_contacts_contribution_id)
-LEFT JOIN compliance USING (surrogate_contacts_contribution_id)
+SELECT surrogate_contacts_contribution_id,
+       contributions.contacts_contribution_id,
+       contributions.surrogate_van_id,
+       contributions.van_id,
+       contributions.contribution_amount,
+       contributions.utc_contribution_received_at,
+       contributions.utc_contribution_created_at,
+       contributions.contribution_status,
+       contributions.financial_program_id,
+       contributions.financial_program,
+       contributions.direct_marketing_code,
+       contributions.utc_contribution_record_modified_at,
+       contributions.online_reference_number,
+       codes.source_code,
+       codes.source_code_description,
+       codes.source_code_path,
+       compliance.contribution_type,
+       compliance.is_memo
+
+  FROM contributions
+  LEFT JOIN codes USING (surrogate_contacts_contribution_id)
+  LEFT JOIN compliance USING (surrogate_contacts_contribution_id)
+
+
 
 
 
