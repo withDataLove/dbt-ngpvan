8f8b61f86dff0f60d6af9ce535b8fcf7ca43f264
Michael Brewster <brew@withdatalove.com>
2024-01-22 12:21:31 +0000

    final?

--- models/intermediate/ngpvan_classic/int_ngpvan_classic__01__contribution_source_codes.sql
+++ models/intermediate/ngpvan_classic/int_ngpvan_classic__01__contribution_source_codes.sql
@@ -11,19 +11,24 @@ codes AS (
 
 most_recent AS (
     SELECT
-        contacts_contribution_id,
-        MAX(utc_created_at) AS utc_created_at,
+        surrogate_contacts_contribution_id,
+        MAX(utc_modified_at) AS utc_modified_at,
         TRUE AS is_most_recent
     FROM contribution_codes
     GROUP BY 1
 )
 
-SELECT *,
+SELECT surrogate_contacts_contribution_id,
+       surrogate_code_id,
+       contribution_codes.contacts_contribution_id,
+       contribution_codes.code_id,
+       contribution_codes.utc_created_at AS utc_source_code_created_at,
+       contribution_codes.utc_modified_at AS utc_source_code_modified_at,
+       codes.committee_id,
+       codes.code AS source_code,
+       codes.code_description AS source_code_description,
+       codes.source_code_path,
        COALESCE(most_recent.is_most_recent, FALSE) AS is_current
 FROM contribution_codes
-LEFT JOIN most_recent USING (contacts_contribution_id, utc_created_at)
-LEFT JOIN codes USING (code_id)
-  
-WHERE  contacts_contribution_id IN (9609269, 9669656)
-
-
+LEFT JOIN most_recent USING (surrogate_contacts_contribution_id, utc_modified_at)
+LEFT JOIN codes USING (surrogate_code_id)
