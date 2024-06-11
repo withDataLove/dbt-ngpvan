641b7f70ab3c804a6f344719aacde8dc76aea988
Michael Brewster <brew@withdatalove.com>
2024-06-11 13:51:57 -0400

    fix two

--- models/staging/ngpvan_classic/stg_ngpvan_classic__contacts_online_forms_responses_single.sql
+++ models/staging/ngpvan_classic/stg_ngpvan_classic__contacts_online_forms_responses_single.sql
@@ -5,7 +5,8 @@ renamed AS (
     SELECT
         contactsonlineformid AS contacts_online_form_id,
         onlineformquestionid AS online_form_question_id,
-        responsevalue AS response_value,
+        onlineformresponseid AS online_form_response_id,
+        otherresponsevalue AS other_response_value,
         createdby AS created_by,
         CONVERT_TIMEZONE('America/New_York', 'UTC', datecreated) AS utc_online_form_response_created_at,
         CONVERT_TIMEZONE('America/New_York', 'UTC', datemodified) AS utc_online_form_response_modified_at,
