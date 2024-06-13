6b5dd33bfdce7d49222d399ccc8fe8aebe714acd
Michael Brewster <brew@withdatalove.com>
2024-06-13 09:42:40 -0400

    add forms enhanced

---
+++ models/intermediate/ngpvan_classic/int_ngpvan_classic__01__online_forms_enhanced.sql
@@ -0,0 +1,27 @@
+with forms as (
+    select *
+    from {{ ref('stg_ngpvan_classic__online_forms') }}
+),
+
+types as (
+    select *
+    from {{ ref('stg_ngpvan_classic__online_form_types') }}
+)
+select
+    forms.online_form_id,
+    types.online_form_type_id,
+    types.online_form_type_name,
+    forms.online_form_name,
+    forms.online_form_url,
+    forms.committee_id,
+    forms.campaign_id,
+    forms.event_id,
+    forms.is_active,
+    confirmation_email_is_enabled,
+    is_recurring_email_enabled,
+    forms.utc_online_form_created_at,
+    forms.utc_online_form_modified_at,
+    forms.surrogate_online_form_id,
+    surrogate_online_form_type_id
+from forms
+join types using(surrogate_online_form_type_id)
