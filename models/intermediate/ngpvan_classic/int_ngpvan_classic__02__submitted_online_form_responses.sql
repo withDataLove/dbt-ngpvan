fac0f2f35a34302bfdace01b630d1f9e7e3d961f
Michael Brewster <brew@withdatalove.com>
2024-06-14 12:00:15 -0400

    update submited responses

--- models/intermediate/ngpvan_classic/int_ngpvan_classic__02__submitted_online_form_responses.sql
+++ models/intermediate/ngpvan_classic/int_ngpvan_classic__02__submitted_online_form_responses.sql
@@ -1,39 +1,31 @@
-{{
-    config(
-        tags=["ngpvan-online-forms"]
-    )
-}}
-
-with submitted_responses as (
-    select * from dbt_mbrewsterffa_dev.stg_ngpvan_classic__contacts_online_forms_responses
+with submitted as (
+    select * from {{ ref('stg_ngpvan_classic__contacts_online_forms_responses') }}
 ),
 
 responses as (
-    select * from dbt_mbrewsterffa_dev.int_ngpvan_classic__02__online_form_responses_enhanced
-),
-
-submitted_forms as (
-    select * from dbt_mbrewsterffa_dev.int_ngpvan_classic__02__submitted_online_forms
-),
-
-questions as (
-    select * from dbt_mbrewsterffa_dev.int_ngpvan_classic__01__online_form_questions_denormalized
+    select * from {{ ref('int_ngpvan_classic__02__online_form_responses_enhanced') }}
 )
 
 select
-    -- submitted_responses.contacts_online_form_id,
-    -- submitted_responses.online_form_question_id,
-    -- submitted_responses.online_form_response_id,
-    -- submitted_responses.response_value,
-    -- submitted_responses.other_response_value,
-    -- -- responses.*
-    -- surrogate_contacts_online_form_id
-    surrogate_contacts_online_forms_response_id, count(*) --, count(distinct surrogate_contacts_online_forms_response_id)
-    
-from submitted_responses
-left join questions using (surrogate_online_form_question_id)
--- left join submitted_forms using (surrogate_contacts_online_form_id)
--- where submitted_forms.online_form_id = 504
--- left join responses using (surrogate_online_form_response_id)
--- where responses.online_form_id = 504
-group by 1 having count(*) > 1
+    submitted.contacts_online_forms_response_id,
+    submitted.contacts_online_form_id,
+    responses.online_form_id,
+    submitted.online_form_question_id,
+    responses.online_form_question_name,
+    responses.online_form_question_display_name,
+    responses.online_form_question_type_name,
+    responses.order_by,
+    submitted.online_form_response_id,
+    responses.online_form_response_name,
+    responses.online_form_response_display_name,
+    responses.is_other_response,
+    submitted.response_value,
+    submitted.other_response_value,
+    submitted.surrogate_contacts_online_forms_response_id,
+    submitted.surrogate_contacts_online_form_id,
+    responses.surrogate_online_form_id,
+    submitted.surrogate_online_form_question_id,
+    surrogate_online_form_response_id
+from submitted
+left join responses using (surrogate_online_form_response_id)
+order by contacts_online_form_id desc, responses.order_by, submitted.online_form_response_id
