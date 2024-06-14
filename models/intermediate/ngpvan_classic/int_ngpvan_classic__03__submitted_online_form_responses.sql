3134f779d2240a32dc9d18e4f49a31634a9ecdcc
Michael Brewster <brew@withdatalove.com>
2024-06-14 14:10:54 -0400

    add back questions

--- models/intermediate/ngpvan_classic/int_ngpvan_classic__03__submitted_online_form_responses.sql
+++ models/intermediate/ngpvan_classic/int_ngpvan_classic__03__submitted_online_form_responses.sql
@@ -8,6 +8,10 @@ with submitted as (
     select * from {{ ref('stg_ngpvan_classic__contacts_online_forms_responses') }}
 ),
 
+questions as (
+    select * from {{ ref('int_ngpvan_classic__01__online_form_questions_denormalized') }}
+),
+
 responses as (
     select * from {{ ref('int_ngpvan_classic__02__online_form_responses_enhanced') }}
 )
@@ -17,10 +21,10 @@ select
     submitted.contacts_online_form_id,
     responses.online_form_id,
     submitted.online_form_question_id,
-    responses.online_form_question_name,
-    responses.online_form_question_display_name,
-    responses.online_form_question_type_name,
-    responses.order_by,
+    questions.online_form_question_name,
+    questions.online_form_question_display_name,
+    questions.online_form_question_type_name,
+    questions.order_by,
     submitted.online_form_response_id,
     responses.online_form_response_name,
     responses.online_form_response_display_name,
@@ -29,9 +33,10 @@ select
     submitted.other_response_value,
     submitted.surrogate_contacts_online_forms_response_id,
     submitted.surrogate_contacts_online_form_id,
-    responses.surrogate_online_form_id,
+    questions.surrogate_online_form_id,
     submitted.surrogate_online_form_question_id,
     surrogate_online_form_response_id
 from submitted
+left join questions using (surrogate_online_form_question_id)
 left join responses using (surrogate_online_form_response_id)
-order by contacts_online_form_id desc, responses.order_by, submitted.online_form_response_id
+order by contacts_online_form_id desc, questions.order_by, submitted.online_form_response_id
